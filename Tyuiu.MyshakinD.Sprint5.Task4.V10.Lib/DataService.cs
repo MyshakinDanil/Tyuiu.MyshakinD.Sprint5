6f4fc6e7df1c199f0fcdc49fa509ef89d9eb5bb5
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-11-23 20:03:33 +0500

    Task4

--- Tyuiu.MyshakinD.Sprint5.Task4.V10.Lib/DataService.cs
+++ Tyuiu.MyshakinD.Sprint5.Task4.V10.Lib/DataService.cs
@@ -5,7 +5,8 @@ namespace Tyuiu.MyshakinD.Sprint5.Task4.V10.Lib {
         public double LoadFromDataFile(string path)
         {
             string str = File.ReadAllText(path);
-            double x = Convert.ToDouble(str);
+            string temp = str.Replace(".", ",");
+            double x = Convert.ToDouble(temp);
             double res = Math.Round(Math.Pow(x, 3) * 1.2 * x + 2, 3);
             return res;
         }
